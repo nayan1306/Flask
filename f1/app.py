0f0673ae76c4b4fbb030ddb56734880f447f8917
nayan1306 <vnayan1306@gmail.com>
2024-07-16 08:05:12 +0530

    Rendering Templates

--- f1/app.py
+++ f1/app.py
@@ -1,43 +1,11 @@
 from flask import Flask
 from markupsafe import escape
 from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Index page"
-
-@app.route("/boost")
-def hello():
-    return "Let\'s make it crazy"
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
-
-
-@app.route('/login', methods=["GET","POST"])
-def login():
-    if request.method == 'POST':
-        return do_the_login()
-    else:
-        return show_the_login_form()
-
-
-def do_the_login():
-    return "Login Done"
-
-def show_the_login_form():
-    return "Login Form"
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):
+    return render_template("index.html",person=name)
