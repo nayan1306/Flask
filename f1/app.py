cefb41118dd654ab50f9c03deb0098136fe7b92c
nayan1306 <vnayan1306@gmail.com>
2024-07-16 11:31:05 +0530

    Redirects and Errors

--- f1/app.py
+++ f1/app.py
@@ -2,10 +2,16 @@ from flask import Flask
 from markupsafe import escape
 from flask import request
 from flask import render_template
+from flask import abort, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello(name=None):
-    return render_template("index.html",person=name)
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    abort(401)
+    this_is_never_executed()
